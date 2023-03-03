be3239a947542009f644c66d7731db51a1c48cbe
kush <200103042091@silveroakuni.ac.in>
2023-03-03 12:45:54 +0530

    updTES

--- src/Pages.js/About.js
+++ src/Pages.js/About.js
@@ -11,9 +11,9 @@ function About (){
             <h1 className="header1">About Us</h1>
             <p className="text1about">Since the 1980s, the push for neoliberalism prioritizes competitiveness and self-reliance as a measurement for economic success. Individuals and their identifying communities that do not meet society's favored neoliberal standards are looked down upon and prone to lower their self-esteem. Some groups who do not fit the preferable neoliberal image are the lower working class and the unemployed.</p>
         </div>
-        <div className="belowimage" >
+        <div className="belowimage" data-aos="fade-up">
         <h1>Gender Equality Should Be Maintained in Each And Every Country And Should Be Accepted By The Government</h1>
-        <img src={aboutyou} alt="..." className="imagebelowabout"/>
+        <img src={aboutyou} alt="..." className="imagebelowabout" data-aos="fade-up"/>
         </div>
         <div>
             <AboutRemote/>
